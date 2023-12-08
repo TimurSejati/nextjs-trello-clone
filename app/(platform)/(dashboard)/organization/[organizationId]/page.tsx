bfb3fbfa2f0b55339a09667af749cf14085c8a04
Timur Dian Radha Sejati <timur7dian84@gmail.com>
2023-12-09 06:22:06 +0700

    [update] iniital update

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,27 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { Suspense } from "react";
+
+import { Separator } from "@/components/ui/separator";
+import { Info } from "./_components/info";
+import { BoardList } from "./_components/board-list";
+// import { checkSubscription } from "@/lib/subscription";
+
+const OrganizationIdPage = () => {
+  // const isPro = await checkSubscription();
+  const { userId, orgId } = auth();
+
+  return (
+    <div className="w-full mb-20">
+      <Info isPro={false} />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <Suspense fallback={<BoardList.Skeleton />}>
+          <BoardList />
+        </Suspense>
+      </div>
+    </div>
+  );
+};
+
+export default OrganizationIdPage;
