f2b6bebdf6c42f070269afd06132948d4b98d126
Timur Dian Radha Sejati <timur7dian84@gmail.com>
2023-12-10 16:06:02 +0700

    [update] final

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -8,10 +8,10 @@ import { createSafeAction } from "@/lib/create-safe-action";
 
 import { InputType, ReturnType } from "./types";
 import { CreateBoard } from "./schema";
-// import { createAuditLog } from "@/lib/create-audit-log";
-// import { ACTION, ENTITY_TYPE } from "@prisma/client";
-// import { incrementAvailableCount, hasAvailableCount } from "@/lib/org-limit";
-// import { checkSubscription } from "@/lib/subscription";
+import { createAuditLog } from "@/lib/create-audit-log";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
+import { incrementAvailableCount, hasAvailableCount } from "@/lib/org-limit";
+import { checkSubscription } from "@/lib/subscription";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -22,15 +22,15 @@ const handler = async (data: InputType): Promise<ReturnType> => {
     };
   }
 
-  // const canCreate = await hasAvailableCount();
-  // const isPro = await checkSubscription();
+  const canCreate = await hasAvailableCount();
+  const isPro = await checkSubscription();
 
-  // if (!canCreate && !isPro) {
-  //   return {
-  //     error:
-  //       "You have reached your limit of free boards. Please upgrade to create more.",
-  //   };
-  // }
+  if (!canCreate && !isPro) {
+    return {
+      error:
+        "You have reached your limit of free boards. Please upgrade to create more.",
+    };
+  }
 
   const { title, image } = data;
 
@@ -64,16 +64,16 @@ const handler = async (data: InputType): Promise<ReturnType> => {
       },
     });
 
-    // if (!isPro) {
-    //   await incrementAvailableCount();
-    // }
+    if (!isPro) {
+      await incrementAvailableCount();
+    }
 
-    // await createAuditLog({
-    //   entityTitle: board.title,
-    //   entityId: board.id,
-    //   entityType: ENTITY_TYPE.BOARD,
-    //   action: ACTION.CREATE,
-    // });
+    await createAuditLog({
+      entityTitle: board.title,
+      entityId: board.id,
+      entityType: ENTITY_TYPE.BOARD,
+      action: ACTION.CREATE,
+    });
   } catch (error) {
     console.log(error);
 
